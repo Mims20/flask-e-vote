dcc9378b2c9e2e763fe621845ae783fc78985902
Mims20 <psterlings@gmail.com>
2023-02-25 07:43:27 -0500

    updated config

--- app/config.py
+++ app/config.py
@@ -2,14 +2,14 @@ from pydantic import BaseSettings
 
 
 class Configs(BaseSettings):
-    database_username: str = "postgres"
-    database_password: str = "0271249352"
-    database_hostname: str = "localhost"
-    database_port: str = "5432"
-    database_name: str = "evote"
-    secret_key: str = "72bee1348d89e37479c47c5e9677771546723dcc2ec7649b969941ca6fe7298e"
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    database_username: str
+    database_password: str
+    database_hostname: str
+    database_port: str
+    database_name: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
         env_file = ".env"
